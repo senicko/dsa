68ccfa69bd2c26029848e5a1fece16b9cfc57440
senicko <25861704+senicko@users.noreply.github.com>
2025-03-23 10:17:49 +0100

    feat: Content Update

--- sorting/problems/dominance.py
+++ sorting/problems/dominance.py
@@ -17,13 +17,13 @@ def dominance(P):
     min_not_dominated = float("inf")
 
     for p in P:
-        # This counts |x| + |y| + |union(x, y)| Every point with
+        # This counts |x| + |y| + |x ∩ y| Every point with
         # both x and y greater equal will be counted twice. This
         # is not a problem however as the point which dominates
-        # most points can't have any point in area |union(x, y)|.
+        # most points can't have any point in area |x ∩ y|.
         not_dominated = x[p[0]] + y[p[1]]
         min_not_dominated = min(min_not_dominated, not_dominated)
 
     # The point dominating most points counted itself twice,
-    # because of adding |union(x, y)|, So we need to subtract 1.
+    # because of adding |x ∩ y|, So we need to subtract 1.
     return n - (min_not_dominated + 1)
