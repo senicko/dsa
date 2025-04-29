b3cf3df745297ff4bc43a954dd51ab39b2bcda88
senicko <25861704+senicko@users.noreply.github.com>
2025-04-29 10:24:26 +0200

    feat: Content Update

--- graphs/labs/lab2/zad4.py
+++ graphs/labs/lab2/zad4.py
@@ -1,38 +1,51 @@
-def compute_finish_order(graph):
+def transpose(graph):
+    n = len(graph)
+    new_graph = [[] for _ in range(n)]
+
+    for u in range(n):
+        for v in graph[u]:
+            new_graph[v].append(u)
+
+    return new_graph
+
+
+def dfs_visit(u, graph, visited, out):
+    for v in graph[u]:
+        if not visited[v]:
+            visited[v] = True
+            dfs_visit(v, graph, visited, out)
+
+    out.append(u)
+
+
+def get_finish_order(graph):
     n = len(graph)
     visited = [False] * n
-    order = []
+    finish_order = []
 
-    def dfs_visit(v):
-        for u in graph[v]:
-            if not visited[u]:
-                visited[u] = True
-                dfs_visit(u)
-        order.append(v)
+    for u in range(n):
+        if not visited[u]:
+            visited[u] = True
+            dfs_visit(u, graph, visited, finish_order)
 
-    for v in range(n):
-        if not visited[n]:
-            visited[v] = True
-            dfs_visit(v)
+    return reversed(finish_order)
 
-    order.reverse()
-    return order
+
+def extract_components(graph, finish_order):
+    n = len(graph)
+    visited = [False] * n
+    components = []
+
+    for u in finish_order:
+        if not visited[u]:
+            visited[u] = True
+            components.append([])
+            dfs_visit(u, graph, visited, components[-1])
+
+    return components
 
 
 def scc(graph):
-    finish_order = compute_finish_order(graph)
-
-    n = len(graph)
-    visited = [False] * n
-
-    def dfs_visit(v):
-        for u in graph[v]:
-            if not visited[u]:
-                visited[u] = True
-                dfs_visit(u)
-
-    for v in finish_order:
-        if not visited[v]:
-
-
-
+    finish_order = get_finish_order(graph)
+    transposed = transpose(graph)
+    return extract_components(transposed, finish_order)
