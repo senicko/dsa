19ddfe0b8797c0dd43f7b931a64e0ad9e25b76cc
senicko <25861704+senicko@users.noreply.github.com>
2025-05-06 09:13:57 +0200

    feat: Content Update

---
+++ graphs/problems/armstrong.py
@@ -0,0 +1,53 @@
+from egz1atesty import runtests
+from queue import PriorityQueue
+from math import inf, floor
+
+
+def build_graph(edges):
+    n = max(map(lambda x: max(x[0], x[1]), edges)) + 1
+    graph = [[] for _ in range(n)]
+
+    for u, v, w in edges:
+        graph[u].append((v, w))
+        graph[v].append((u, w))
+
+    return graph
+
+
+def dijkstra(graph, s):
+    n = len(graph)
+
+    dist = [inf] * n
+    queue = PriorityQueue()
+
+    dist[s] = 0
+    queue.put((0, s))
+
+    while not queue.empty():
+        d, u = queue.get()
+
+        if d > dist[u]:
+            continue
+
+        for v, w in graph[u]:
+            alt = dist[u] + w
+            if dist[v] > alt:
+                dist[v] = alt
+                queue.put((alt, v))
+
+    return dist
+
+
+def armstrong(B, G, s, t):
+    graph = build_graph(G)
+    dist_s_t = dijkstra(graph, s)
+    dist_t_s = dijkstra(graph, t)
+    result = dist_s_t[t]
+
+    for u, p, q in B:
+        result = min(result, dist_s_t[u] + dist_t_s[u] * (p / q))
+
+    return floor(result)
+
+
+runtests(armstrong, all_tests=True)
